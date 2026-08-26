7f9c8dc077856e992f89fe89fc5fbdb19f3f1dc1
sant-mell <173726620+sant-mell@users.noreply.github.com>
2026-08-25 20:10:36 -0600

    added working dev

--- src/posts.tsx
+++ src/posts.tsx
@@ -2,7 +2,17 @@
 import useMediaQuery from "@mui/material/useMediaQuery";
 import type { Theme } from "@mui/material";
 import type { ListProps } from "react-admin";
-import { List, DataTable, SimpleList, TextField } from "react-admin";
+import {
+  List,
+  DataTable,
+  SimpleList,
+  TextField,
+  Create,
+  SimpleForm,
+  ReferenceInput,
+  SelectInput,
+  TextInput,
+} from "react-admin";
 
 export const PostList = (props: ListProps) => {
   const isSmall = useMediaQuery<Theme>(theme => theme.breakpoints.down("sm"));
@@ -24,3 +34,15 @@ export const PostList = (props: ListProps) => {
     </List>
   );
 };
+
+export const PostCreate = () => (
+  <Create>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput required source="body" multiline rows={5} />
+    </SimpleForm>
+  </Create>
+);
